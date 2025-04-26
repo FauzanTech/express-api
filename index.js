ae81ff9b2a7819669cee49e9b01dacec9932b930
Fauzan <zanf518@gmail.com>
2025-04-26 09:13:01 +0800

    fauzan ganteng

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.port || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Express on Railway!");
+  res.send("Fauzan Ganteng");
 });
 
 app.get("/api", (req, res) => {
